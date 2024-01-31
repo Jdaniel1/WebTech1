df1fce8a1984c738d68ecaed3ebe2965a0f2b48c
Jdaniel1 <79257023+Jdaniel1@users.noreply.github.com>
2024-02-01 00:52:10 +0100

    Create script_btn.js

---
+++ script/script_btn.js
@@ -0,0 +1,42 @@
+var piano1 = document.getElementById("btn_Click_piano_1");
+function Click_piano_1() {
+    piano1.innerHTML = "Sikeres vásárlás!";
+    piano1.style.color = 'whitesmoke';
+    piano1.style.fontWeight = 'bolder';
+}
+
+var piano2 = document.getElementById("btn_Click_piano_2");
+function Click_piano_2() {
+    piano2.innerHTML = "Sikeres vásárlás!";
+    piano2.style.color = 'whitesmoke';
+    piano2.style.fontWeight = 'bolder';  
+}
+
+var piano3 = document.getElementById("btn_Click_piano_3");
+function Click_piano_3() {
+    piano3.innerHTML = "Sikeres vásárlás!";
+    piano3.style.color = 'whitesmoke';
+    piano3.style.fontWeight = 'bolder';
+}
+
+
+var guitar1 = document.getElementById("btn_Click_guitar_1");
+function Click_guitar_1() {
+    guitar1.innerHTML = "Sikeres vásárlás!";
+    guitar1.style.color = 'whitesmoke';
+    guitar1.style.fontWeight = 'bolder';
+}
+
+var guitar2 = document.getElementById("btn_Click_guitar_2");
+function Click_guitar_2() {
+    guitar2.innerHTML = "Sikeres vásárlás!";
+    guitar2.style.color = 'whitesmoke';
+    guitar2.style.fontWeight = 'bolder';  
+}
+
+var guitar3 = document.getElementById("btn_Click_guitar_3");
+function Click_guitar_3() {
+    guitar3.innerHTML = "Sikeres vásárlás!";
+    guitar3.style.color = 'whitesmoke';
+    guitar3.style.fontWeight = 'bolder';
+}
